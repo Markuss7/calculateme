c40c8833b6c6a86d46795efd0a77df8c26d738e6
Markuss7 <Markuss7@users.noreply.github.com>
2026-06-15 22:27:57 +0200

    fixing bug for pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/calculateme/',
   plugins: [react()],
   define: {
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version ?? '0.0.0'),
